aac07aa44b18300da37e0726a7b2d185b8188ed4
Valeriia Klimova <valeriia@MacBook-Pro.netis>
2020-07-06 17:38:40 +0300

    Updates

---
+++ precache-manifest.80e9a6f86b1e8003016064c06150ac4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e9dfcdce5e6f81d804056772e1c2b8",
+    "url": "/test-task/index.html"
+  },
+  {
+    "revision": "ab7551b427534084df80",
+    "url": "/test-task/static/css/main.7db1c44e.chunk.css"
+  },
+  {
+    "revision": "6a9682420ee5f7b1a8d5",
+    "url": "/test-task/static/js/2.b1f60475.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-task/static/js/2.b1f60475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab7551b427534084df80",
+    "url": "/test-task/static/js/main.a43546d9.chunk.js"
+  },
+  {
+    "revision": "2f429c409b5039645161",
+    "url": "/test-task/static/js/runtime-main.5becff4d.js"
+  }
+]);
